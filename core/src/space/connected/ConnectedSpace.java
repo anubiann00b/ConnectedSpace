767051cbc4cc9fd64de62ec0a08dc90a496f5fb5
Shreyas <skraman1999@gmail.com>
2015-02-14 22:07:42 -0800

    Tabs to spaces.

--- core/src/space/connected/ConnectedSpace.java
+++ core/src/space/connected/ConnectedSpace.java
@@ -11,16 +11,16 @@ import java.util.List;
 
 public class ConnectedSpace extends ApplicationAdapter {
 
-	SpriteBatch batch;
+    SpriteBatch batch;
     Animation player;
     float cumulativeTime = 0;
     float px = 240;
     float py = 240;
     List<Star> stars = new ArrayList<Star>();
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
         player = SpriteSheet.SHIP_VARIANT_1.getAnim(16);
         player.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
 
@@ -28,19 +28,19 @@ public class ConnectedSpace extends ApplicationAdapter {
             stars.add(new Star());
     }
 
-	@Override
-	public void render() {
+    @Override
+    public void render() {
         px += (float) (10.0*Math.cos(Math.toRadians(Gdx.input.getRoll()-90)));
         py += (float) (10.0*Math.sin(Math.toRadians(Gdx.input.getPitch())));
 
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
 
         for (Star s : stars)
             s.render(batch);
         batch.draw(player.getKeyFrame(cumulativeTime+=2), this.px, this.py, 25 * 4, 30 * 4);
 
         batch.end();
-	}
+    }
 }
