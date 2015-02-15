5da92d1d2e79603dfdb21f487f5818bc125d1065
Shreyas <skraman1999@gmail.com>
2015-02-14 22:06:57 -0800

    Stars!

--- core/src/space/connected/ConnectedSpace.java
+++ core/src/space/connected/ConnectedSpace.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConnectedSpace extends ApplicationAdapter {
 
 	SpriteBatch batch;
@@ -13,21 +16,20 @@ public class ConnectedSpace extends ApplicationAdapter {
     float cumulativeTime = 0;
     float px = 240;
     float py = 240;
+    List<Star> stars = new ArrayList<Star>();
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
         player = SpriteSheet.SHIP_VARIANT_1.getAnim(16);
         player.setPlayMode(Animation.PlayMode.LOOP_PINGPONG);
-	}
+
+        for (int i=0;i<20;i++)
+            stars.add(new Star());
+    }
 
 	@Override
 	public void render() {
-        float delta = 1;
-        if (Gdx.graphics.getFramesPerSecond() != 0)
-            delta = (float) Game.FPS/Gdx.graphics.getFramesPerSecond();
-        cumulativeTime += delta*2;
-
         px += (float) (10.0*Math.cos(Math.toRadians(Gdx.input.getRoll()-90)));
         py += (float) (10.0*Math.sin(Math.toRadians(Gdx.input.getPitch())));
 
@@ -35,7 +37,9 @@ public class ConnectedSpace extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
-        batch.draw(player.getKeyFrame(cumulativeTime), this.px, this.py, 25 * 4, 30 * 4);
+        for (Star s : stars)
+            s.render(batch);
+        batch.draw(player.getKeyFrame(cumulativeTime+=2), this.px, this.py, 25 * 4, 30 * 4);
 
         batch.end();
 	}
---
+++ core/src/space/connected/Star.java
@@ -0,0 +1,29 @@
+package space.connected;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Star {
+
+    Sprite sprite;
+    double x = 0;
+    double y = -1;
+    double z = 0;
+
+    public Star() {
+        sprite = new Sprite(new Texture("star.png"));
+    }
+
+    public void render(SpriteBatch batch) {
+        y += z;
+        if (y < 0) {
+            x = Math.random()*Gdx.graphics.getWidth();
+            z = -(int)(4+Math.random()*5);
+            y = 2*Math.random()*Gdx.graphics.getHeight();
+        }
+        sprite.setPosition((float)x, (float)y);
+        sprite.draw(batch);
+    }
+}
